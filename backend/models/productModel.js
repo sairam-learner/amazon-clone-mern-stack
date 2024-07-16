51cf8c4690df363db9fd0bb84a07693a4273e197
sairam-learner <molugusairam521@gmail.com>
2024-07-16 16:37:41 +0530

    Add files via upload

---
+++ backend/models/productModel.js
@@ -0,0 +1,38 @@
+import mongoose from 'mongoose';
+const reviewSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    comment: { type: String, required: true },
+    rating: { type: Number, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+const productSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true, unique: true },
+    _seller: { type: mongoose.Schema.Types.ObjectID, ref: 'User' },
+    get seller() {
+      return this._seller;
+    },
+    set seller(value) {
+      this._seller = value;
+    },
+    image: { type: String, required: true },
+    brand: { type: String, required: true },
+    category: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true },
+    countInStock: { type: Number, required: true },
+    rating: { type: Number, required: true },
+    numReviews: { type: Number, required: true },
+    reviews: [reviewSchema],
+  },
+  {
+    timestamps: true,
+  }
+);
+const Product = mongoose.model('Product', productSchema);
+
+export default Product;
